eeefa6c23342f64e839fe7e7f1d61897de7925c4
Hamburger Réka <Rka>
2022-09-11 20:23:14 +0200

    got

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-function myFunction() {
-    document.getElementById("panel").style.display = "block";
-  }
+
+//Ez a 100. verzió, amit probálok. sajnos nem tudtam megoldani:( az aside-ot.
+
+
+
+let got = async() => {
+
+  //let bioname =json.parse('../got.json');
+  let bioname = await fetch('../json/got.json');
+  let result = await bioname.json();
+  
+  for (let i=0; i<result.length; i++) {
+   let pname= document.getElementsByClassName('pp');
+    if (pname ==result[i].name){
+
+      return document.getElementsByClassName('leiras').innerHTML=result[i].bio;
+    };
+  };
+};
